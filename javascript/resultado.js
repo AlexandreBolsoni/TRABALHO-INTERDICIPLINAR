b6cdb09a2aa217d2ad7864a88f14f291e0ed1e31
AlexandreBolsoni <104595251+AlexandreBolsoni@users.noreply.github.com>
2022-07-25 21:19:23 -0300

    reset.css ta aki

--- javascript/resultado.js
+++ javascript/resultado.js
@@ -1,34 +1,33 @@
+var inBuscar = document.getElementById("inBuscar");
+var inData = document.getElementById("inData");
+function vetor() {
 
-var buscar = document.getElementById("inBuscar");
-//buscar.addEventListener("keyup", pesquisar);
-var btMostrar = document.getElementById("btMostrar");
+    for (var aux = 0; aux < nome.length; aux++) {
 
-function vetor(){
-   
-    for(var aux  = 0; aux < Nome.length; aux++){
-      
         var mae = document.getElementById("tabela");
         var organiza = document.createElement('tr');
 
+       
         var vetNome = document.createElement('td');
         var vetCPF = document.createElement('td');
         var vetNascimento = document.createElement('td');
         var vetPonto = document.createElement('td');
-        var resultado = document.createElement("td")
+        var resultado = document.createElement("td");
         
 
-        vetNome.textContent = Nome[aux];
-        vetCPF.textContent = CPF[aux];
-        vetNascimento.textContent = Nascimento[aux];
-        vetPonto.textContent = Ponto[aux];
-        if(Ponto[aux] >= 900){
+      
+        vetNome.textContent = nome[aux];
+        vetCPF.textContent = cpf[aux];
+        vetNascimento.textContent = nascimento[aux];
+        vetPonto.textContent = ponto[aux];
+        if (ponto[aux] >= 900) {
             resultado.textContent = "APROVADO"
-        }else{
+        } else {
             resultado.textContent = "REPROVADO"
         }
-       
-      
+        
 
+        
         organiza.appendChild(vetNome);
         organiza.appendChild(vetCPF);
         organiza.appendChild(vetNascimento);
@@ -36,70 +35,135 @@ function vetor(){
         organiza.appendChild(resultado);
         
 
+
         mae.appendChild(organiza);
-       
+
+        
+
+    }
+}
+
+
+function pesquisar() {
+    var busca = inBuscar.value
+    var mae = document.getElementById("tabela");
+    mae.innerText = ""
+    for (var i = 0; i < nome.length; i++) {
+        if (nome[i].toUpperCase().indexOf(busca.toUpperCase()) != -1) {
+            var organiza = document.createElement('tr');
+
+            var vetNome = document.createElement('td');
+            var vetCPF = document.createElement('td');
+            var vetNascimento = document.createElement('td');
+            var vetPonto = document.createElement('td');
+            var resultado = document.createElement("td");
+
+
+            vetNome.textContent = nome[i];
+            vetCPF.textContent = cpf[i];
+            vetNascimento.textContent = nascimento[i];
+            vetPonto.textContent = ponto[i];
+            if (ponto[i] >= 900) {
+                resultado.textContent = "APROVADO"
+            } else {
+                resultado.textContent = "REPROVADO"
+            }
+
+
+
+            organiza.appendChild(vetNome);
+            organiza.appendChild(vetCPF);
+            organiza.appendChild(vetNascimento);
+            organiza.appendChild(vetPonto);
+            organiza.appendChild(resultado);
+
+
+            mae.appendChild(organiza);
+
+        }
+
+    }
+}
+inBuscar.addEventListener("keyup",pesquisar);
+
+
+function mostra() {
+   
+    var busca = inData.value
+    var mae = document.getElementById("tabela");
+    mae.innerText = ""
+    for (var i = 0; i < anoNascimento.length; i++) {
 
       
-    }
-}
+            if (anoNascimento[i]<=busca) {
+               
+                var organiza = document.createElement('tr');
 
-function pesquisar(){
-    
-    var coluna = "0";
+                var vetNome = document.createElement('td');
+                var vetCPF = document.createElement('td');
+                var vetNascimento = document.createElement('td');
+                var vetPonto = document.createElement('td');
+                var resultado = document.createElement("td");
+                
 
-    var buscar, tab, tr, th, td, i;
+                
+                vetNome.textContent = nome[i];
+                vetCPF.textContent = cpf[i];
+                vetNascimento.textContent = nascimento[i];
+                vetPonto.textContent = ponto[i];
+                if (ponto[i] >= 900) {
+                    resultado.textContent = "APROVADO"
+                } else {
+                    resultado.textContent = "REPROVADO"
+                }
+                
 
-    buscar = document.getElementById("inBuscar");
-    buscar = buscar.value.toUpperCase();
 
-    tab = document.getElementById("tab");
 
-    tr = tab.getElementsByTagName("tr");
-    th = tab.getElementsByTagName("th");
+                organiza.appendChild(vetNome);
+                organiza.appendChild(vetCPF);
+                organiza.appendChild(vetNascimento);
+                organiza.appendChild(vetPonto);
+                organiza.appendChild(resultado);
+                
 
-    for(var i = 0;i<tr.length;i++){
-        td=tr[i].getElementsByTagName("td")[coluna];
-        if(td){
 
-            if(td.innerHTML.toUpperCase().indexOf(buscar) > -1){
-                tr[i].style.display ="";
-
-            }else{
-                tr[i].style.display ="none";
+                mae.appendChild(organiza);
             }
-        }
-    }
+            if(busca == ""){
+                var organiza = document.createElement('tr');
 
-}
+            var vetNome = document.createElement('td');
+            var vetCPF = document.createElement('td');
+            var vetNascimento = document.createElement('td');
+            var vetPonto = document.createElement('td');
+            var resultado = document.createElement("td");
 
-function mostra(){
-    
-    var coluna = "2";
 
-    var buscar, tab, tr, th, td, i;
-
-    buscar = document.getElementById("inData");
-    buscar = buscar.value.toUpperCase();
-
-    tab = document.getElementById("tab");
-
-    tr = tab.getElementsByTagName("tr");
-    th = tab.getElementsByTagName("th");
-
-    for(var i = 0;i<=inData.value;i++){
-        td=tr[i].getElementsByTagName("td")[coluna];
-        if(td){
-
-            if(td.innerHTML.toUpperCase().indexOf(buscar) > -1){
-                tr[i].style.display ="";
-
-            }else{
-                tr[i].style.display ="none";
+            vetNome.textContent = nome[i];
+            vetCPF.textContent = cpf[i];
+            vetNascimento.textContent = nascimento[i];
+            vetPonto.textContent = ponto[i];
+            if (ponto[i] >= 900) {
+                resultado.textContent = "APROVADO"
+            } else {
+                resultado.textContent = "REPROVADO"
             }
+
+
+
+            organiza.appendChild(vetNome);
+            organiza.appendChild(vetCPF);
+            organiza.appendChild(vetNascimento);
+            organiza.appendChild(vetPonto);
+            organiza.appendChild(resultado);
+
+
+            mae.appendChild(organiza);
+
+            }
+        
         }
-    }
 
 }
-
-
-
+inData.addEventListener("keyup",mostra);
